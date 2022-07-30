673539e1ffe8b1f7e93e029b3f3061361694dea3
Ly Quoc  Phong <lyquocphong@gmail.com>
2022-07-30 23:15:56 +0300

    Add helper functions and use it in service

--- src/payment/bestpayments/bestpayments.service.ts
+++ src/payment/bestpayments/bestpayments.service.ts
@@ -6,25 +6,25 @@ export class BestpaymentsService {
 
     constructor(private bestPaymentsHelper: BestpaymentsHelper) {}
 
-
     /**
      * Create payment in bestpayments provider
      */
     createPayment() {
         const webhookCallbackUrl: string = this.bestPaymentsHelper.generateCallbackUrl();
+        const url:string = this.bestPaymentsHelper.getCreatePaymentEndpointUrl();
     }
 
     /**
      * Get payment in bestpayments provider
      */
     getPayment() {
-
+        const url:string = this.bestPaymentsHelper.getFindPaymentEndpointUrl();
     }
 
     /**
      * Capture payment in bestpayments provider
      */
     capturePayment() {
-
+        const url:string = this.bestPaymentsHelper.getCapturePaymentEndpointUrl();
     }
 }
