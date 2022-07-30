6bde7e6251d3ab031d03a765506177f0eb78f5bc
Ly Quoc  Phong <lyquocphong@gmail.com>
2022-07-30 22:01:26 +0300

    Add config module and swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  console.log(process.env);
+
+  const config = new DocumentBuilder()
+    .setTitle(process.env.SWAGGER_TITLE)
+    .setDescription(process.env.SWAGGER_DESC)
+    .setVersion(process.env.SWAGGER_VERSION)
+    //.addTag(process.env.SWAGGER_TAG)
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
