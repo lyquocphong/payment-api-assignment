0f3c3bf287c7d6e473fe88c33c760dac8add4c86
Ly Quoc  Phong <lyquocphong@gmail.com>
2022-07-30 22:17:17 +0300

    Remove console.log and commented code

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  console.log(process.env);
-
   const config = new DocumentBuilder()
     .setTitle(process.env.SWAGGER_TITLE)
     .setDescription(process.env.SWAGGER_DESC)
     .setVersion(process.env.SWAGGER_VERSION)
-    //.addTag(process.env.SWAGGER_TAG)
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup(process.env.SWAGGER_ENDPOINT_PREFIX, app, document);
